b53f8a0ed4515609173e7e90478e202beb2927a2
Matthew Renfer <mr.renfer@gmail.com>
2025-05-18 13:05:47 -0400

    edits

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,75 +1,94 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 export default class About extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <React.Fragment>
-        <section id="about" className="s-about target-section">
-          <div className="row">
-            <div className="column large-3 tab-12">
-              <img
-                className="s-about__pic"
-                src="images/matt-profile.jpg"
-                alt=""
-              />
-            </div>
-            <div className="column large-9 tab-12 s-about__content">
-              <h3>About Me</h3>
-              {/* <p>{resumeData.aboutMe}</p> */}
-              <p>
-              I have always been a problem-solver who thinks in logical, systematic ways&mdash;engaging the creative process along the way. A big part of what I love in my career is birthing ideas into practical, impactful solutions.
-              </p>
-              <p>I&apos;ve been making websites since the early dawn of the internet. A lot has changed since then, which is why I am always eager to learn newer technologies and expand my web development toolkit, keeping companies and organizations ahead of the curve.
-              </p>
-              <p>Creating intuitive user experiences is important to me. The journey a user takes through a website should be easy to follow and even enjoyable.
-              </p>
-              <p>Collaboration and positivity are central to how I work. Great results come from great teams, and I value environments where efficiency meets balance. After all, it&apos;s not just about the work we do, but how we do it.
-              </p>
-              <hr />
-              <div className="row s-about__content-bottom">
-                <div className="column w-1000-stack">
-                  <h3>Contact Details</h3>
-                  <p>
-                    <a
-                      href={resumeData.facebookMessenger}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Message Me on Facebook
-                    </a>
-                    <br />
-                    {resumeData.email}
-                  </p>
-                </div>
-                <div className="column w-1000-stack">
-                  <a
-                    href={`${resumeData.publicUrl}/resume-2024.pdf${resumeData.resumeVersion}`}
-                    className="btn btn--download"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      style={{
-                        fill: "rgba(0, 0, 0, 1)",
-                        transform: "",
-                        msFilter: "",
-                      }}
-                    >
-                      <path d="M12 16L16 11 13 11 13 4 11 4 11 11 8 11z" />
-                      <path d="M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z" />
-                    </svg>
-                    View CV
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>{" "}
-        </section>
-      </React.Fragment>
-    );
-  }
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <React.Fragment>
+                <section id='about' className='s-about target-section'>
+                    <div className='row'>
+                        <div className='column large-3 tab-12'>
+                            <img
+                                className='s-about__pic'
+                                src='images/matt-profile.jpg'
+                                alt=''
+                            />
+                        </div>
+                        <div className='column large-9 tab-12 s-about__content'>
+                            <h3>About Me</h3>
+                            {/* <p>{resumeData.aboutMe}</p> */}
+                            <p>
+                                I have always been a problem-solver who thinks
+                                in logical, systematic ways&mdash;engaging the
+                                creative process along the way. A big part of
+                                what I love in my career is birthing ideas into
+                                practical, impactful solutions.
+                            </p>
+                            <p>
+                                I&apos;ve been making websites since the early
+                                dawn of the internet. A lot has changed since
+                                then, which is why I am always eager to learn
+                                newer technologies and expand my web development
+                                toolkit, keeping companies and organizations
+                                ahead of the curve.
+                            </p>
+                            <p>
+                                Creating intuitive user experiences is important
+                                to me. The journey a user takes to get where
+                                they need to go should be easy to follow and
+                                even enjoyable.
+                            </p>
+                            <p>
+                                Collaboration and positivity are central to how
+                                I work. Great results come from great teams, and
+                                I value environments where efficiency meets
+                                balance. After all, it&apos;s not just about the
+                                work we do, but how we do it.
+                            </p>
+                            <hr />
+                            <div className='row s-about__content-bottom'>
+                                <div className='column w-1000-stack'>
+                                    <h3>Contact Details</h3>
+                                    <p>
+                                        <a
+                                            href={resumeData.facebookMessenger}
+                                            target='_blank'
+                                            rel='noreferrer'
+                                        >
+                                            Message Me on Facebook
+                                        </a>
+                                        <br />
+                                        {resumeData.email}
+                                    </p>
+                                </div>
+                                <div className='column w-1000-stack'>
+                                    <a
+                                        href={`${resumeData.publicUrl}/resume-2024.pdf${resumeData.resumeVersion}`}
+                                        className='btn btn--download'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <svg
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            width={24}
+                                            height={24}
+                                            viewBox='0 0 24 24'
+                                            style={{
+                                                fill: 'rgba(0, 0, 0, 1)',
+                                                transform: '',
+                                                msFilter: '',
+                                            }}
+                                        >
+                                            <path d='M12 16L16 11 13 11 13 4 11 4 11 11 8 11z' />
+                                            <path d='M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z' />
+                                        </svg>
+                                        View CV
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>{' '}
+                </section>
+            </React.Fragment>
+        );
+    }
 }
--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,76 +1,95 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 export default class Hero extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <React.Fragment>
-        <section id="hero" className="s-hero target-section">
-          <div className="s-hero__bg rellax" data-rellax-speed={-7} />
-          <div className="row s-hero__content">
-            <div className="column">
-              <div className="s-hero__content-about">
-                <h1>{resumeData.name}</h1>
-                <h2>
-                  <em>{resumeData.role}</em>
-                </h2>
-                <br />
-                <h3>
-                  I&apos;m an {resumeData.city}-based{" "}
-                  <span>front-end developer</span> &amp;{" "}
-                  <span>UI/UX designer</span> with more than 15 years of
-                  experience creating intuitive digital user experiences.
-                </h3>
-                <div className="s-hero__content-social">
-                  <a
-                    href={resumeData.linkedin}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-linkedin" aria-hidden="true" />
-                  </a>
-                  <a href={resumeData.github} target="_blank" rel="noreferrer">
-                    <i className="fab fa-github-square" aria-hidden="true" />
-                  </a>
-                  <a
-                    href={resumeData.facebook}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-facebook-square" aria-hidden="true" />
-                  </a>
-                  <a
-                    href={resumeData.instagram}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-instagram" aria-hidden="true" />
-                  </a>
-                </div>
-              </div>{" "}
-            </div>
-          </div>{" "}
-          <div className="s-hero__scroll">
-            <a href="#about" className="s-hero__scroll-link smoothscroll">
-              <span className="scroll-arrow">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  style={{
-                    fill: "rgba(0, 0, 0, 1)",
-                    transform: "",
-                    msFilter: "",
-                  }}
-                >
-                  <path d="M18.707 12.707L17.293 11.293 13 15.586 13 6 11 6 11 15.586 6.707 11.293 5.293 12.707 12 19.414z" />
-                </svg>
-              </span>
-              <span className="scroll-text">Scroll Down</span>
-            </a>
-          </div>{" "}
-        </section>{" "}
-      </React.Fragment>
-    );
-  }
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <React.Fragment>
+                <section id='hero' className='s-hero target-section'>
+                    <div className='s-hero__bg rellax' data-rellax-speed={-7} />
+                    <div className='row s-hero__content'>
+                        <div className='column'>
+                            <div className='s-hero__content-about'>
+                                <h1>{resumeData.name}</h1>
+                                <h2>
+                                    <em>{resumeData.role}</em>
+                                </h2>
+                                <br />
+                                <h3>
+                                    <span>Collaborative</span> developer & UX
+                                    strategist with 15+ years of experience
+                                    delivering <span>intuitive, scalable</span>{' '}
+                                    digital solutions.
+                                </h3>
+                                <div className='s-hero__content-social'>
+                                    <a
+                                        href={resumeData.linkedin}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <i
+                                            className='fab fa-linkedin'
+                                            aria-hidden='true'
+                                        />
+                                    </a>
+                                    <a
+                                        href={resumeData.github}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <i
+                                            className='fab fa-github-square'
+                                            aria-hidden='true'
+                                        />
+                                    </a>
+                                    <a
+                                        href={resumeData.facebook}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <i
+                                            className='fab fa-facebook-square'
+                                            aria-hidden='true'
+                                        />
+                                    </a>
+                                    <a
+                                        href={resumeData.instagram}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <i
+                                            className='fab fa-instagram'
+                                            aria-hidden='true'
+                                        />
+                                    </a>
+                                </div>
+                            </div>{' '}
+                        </div>
+                    </div>{' '}
+                    <div className='s-hero__scroll'>
+                        <a
+                            href='#about'
+                            className='s-hero__scroll-link smoothscroll'
+                        >
+                            <span className='scroll-arrow'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width={24}
+                                    height={24}
+                                    viewBox='0 0 24 24'
+                                    style={{
+                                        fill: 'rgba(0, 0, 0, 1)',
+                                        transform: '',
+                                        msFilter: '',
+                                    }}
+                                >
+                                    <path d='M18.707 12.707L17.293 11.293 13 15.586 13 6 11 6 11 15.586 6.707 11.293 5.293 12.707 12 19.414z' />
+                                </svg>
+                            </span>
+                            <span className='scroll-text'>Scroll Down</span>
+                        </a>
+                    </div>{' '}
+                </section>{' '}
+            </React.Fragment>
+        );
+    }
 }
